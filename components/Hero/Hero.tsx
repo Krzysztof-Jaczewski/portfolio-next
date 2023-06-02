3586d318de766bfadb2753fe1d2e9918090e6314
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-06-02 19:26:57 +0200

    dev: implemented link button to Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,25 +3,22 @@ import styles from './hero.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import devImage from '/public/images/dev.png';
+import { LinkButton } from 'components/LinkButton';
 
-const Hero: FC = () => {
-    return (
-        <section className={styles.header}>
-            <div>
-                <h2>Krzysztof Jaczewski</h2>
-                <h3>frond-end designer</h3>
-                <Link href='/#projects' className={styles.button}>
-                    Check projects
-                </Link>
-            </div>
-            <Image
-                className={styles.image}
-                src={devImage}
-                alt='devImage'
-                placeholder='blur'
-            />
-        </section>
-    );
-};
+const Hero: FC = () => (
+    <div className={styles.header}>
+        <div>
+            <h2>Krzysztof Jaczewski</h2>
+            <h3>Frond-end developer</h3>
+            <LinkButton text='zobacz projekty' link='/#projects' />
+        </div>
+        <Image
+            className={styles.image}
+            src={devImage}
+            alt='devImage'
+            placeholder='blur'
+        />
+    </div>
+);
 
 export default Hero;
