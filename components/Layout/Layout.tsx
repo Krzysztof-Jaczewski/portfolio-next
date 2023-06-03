09063abcc716d9c4b7a801b500ff48d3636ef0ec
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-06-03 17:59:28 +0200

    dev: modified layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import { FC, ReactNode } from 'react';
 import styles from './layout.module.scss';
+import { ParallaxBackground } from 'components/ParallaxBackground';
+import { Footer } from 'components/Footer';
 
 interface Props {
     children: ReactNode;
@@ -8,7 +10,9 @@ interface Props {
 const Layout: FC<Props> = ({ children }) => {
     return (
         <div className={styles.siteWrapper}>
+            <ParallaxBackground />
             <main>{children}</main>
+            <Footer />
         </div>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,19 @@
 import type { GetStaticProps, NextPage } from 'next';
 import getRepositories from '../modules/getRepositories/getRepositories';
-import { allowedRepos } from 'configs/allowedRepos';
 import { RepoType } from 'modules/getRepositories/types';
 import { Hero } from 'components/Hero';
 import { Layout } from 'components/Layout';
 import { Projects } from 'components/Projects';
-import { ParallaxBackground } from 'components/ParallaxBackground';
 
 interface Props {
     repos: RepoType;
 }
 
 const Home: NextPage<Props> = ({ repos }) => (
-    <>
-        <ParallaxBackground />
-        <Layout>
-            <Hero />
-            <Projects repos={repos} />
-        </Layout>
-    </>
+    <Layout>
+        <Hero />
+        <Projects repos={repos} />
+    </Layout>
 );
 
 export default Home;
