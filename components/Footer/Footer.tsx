67fc0be86ed60a0cd752d12bf2f2cd65abac8344
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-06-03 17:59:15 +0200

    dev: added footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { FC, ReactNode } from 'react';
+import styles from './footer.module.scss';
+
+const Footer: FC = () => (
+    <footer className={styles.footerWrapper}>
+        <p>Made with passion and love💖 by EyeCode Krzysztof Jaczewski</p>
+    </footer>
+);
+
+export default Footer;
