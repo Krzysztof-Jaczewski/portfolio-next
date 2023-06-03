77f70c8266f82e4ef403f1a6ea6a4ef04ba0bc99
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-06-03 19:14:07 +0200

    dev: moved svg filter out of LinkButton

--- components/LinkButton/LinkButton.tsx
+++ components/LinkButton/LinkButton.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import styles from './linkButton.module.scss';
 import Link from 'next/link';
 import clsx from 'clsx';
+import SvgFilter from 'components/LinkButton/svgfilter';
 
 interface Props {
     link: string;
@@ -12,29 +13,7 @@ interface Props {
 const LinkButton: FC<Props> = ({ link, text, className }) => (
     <Link href={link} className={clsx(styles.button, className)}>
         {text}
-        <svg xmlns='http://www.w3.org/2000/svg' version='1.1'>
-            <defs>
-                <filter id='gooey'>
-                    <feGaussianBlur
-                        in='SourceGraphic'
-                        stdDeviation='5'
-                        result='blur'
-                    />
-                    <feColorMatrix
-                        in='blur'
-                        type='matrix'
-                        values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9'
-                        result='highContrastGraphic'
-                    />
-                    <feComposite
-                        in='SourceGraphic'
-                        in2='highContrastGraphic'
-                        operator='atop'
-                    />
-                </filter>
-            </defs>
-        </svg>
-
+        <SvgFilter />
         <span className={styles.bubbles}>
             {[...Array(10)].map((_, index) => (
                 <span key={index} className={styles.bubble} />
