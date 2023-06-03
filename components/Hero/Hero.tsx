99c82a23316b3abe91158c0552159a53654ccda8
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-06-03 17:39:41 +0200

    dev: modified Hero responsiveness

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,23 +1,23 @@
 import { FC } from 'react';
 import styles from './hero.module.scss';
-import Link from 'next/link';
 import Image from 'next/image';
 import devImage from '/public/images/dev.png';
 import { LinkButton } from 'components/LinkButton';
 
 const Hero: FC = () => (
     <div className={styles.header}>
-        <div>
-            <h2>Krzysztof Jaczewski</h2>
-            <h3>Frond-end developer</h3>
-            <LinkButton text='zobacz projekty' link='/#projects' />
+        <div className={styles.intro}>
+            <h1>Krzysztof Jaczewski</h1>
+            <h2>Frond-end developer</h2>
+            <LinkButton
+                className={styles.button}
+                text='Projekty'
+                link='/#projects'
+            />
         </div>
-        <Image
-            className={styles.image}
-            src={devImage}
-            alt='devImage'
-            placeholder='blur'
-        />
+        <div className={styles.image}>
+            <Image src={devImage} alt='devImage' placeholder='blur' />
+        </div>
     </div>
 );
 
