c229e43aad7582a7c7a1cb696cf5a62149e8c296
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-06-03 18:59:18 +0200

    dev: modified linked buttons

--- components/LinkButton/LinkButton.tsx
+++ components/LinkButton/LinkButton.tsx
@@ -12,6 +12,34 @@ interface Props {
 const LinkButton: FC<Props> = ({ link, text, className }) => (
     <Link href={link} className={clsx(styles.button, className)}>
         {text}
+        <svg xmlns='http://www.w3.org/2000/svg' version='1.1'>
+            <defs>
+                <filter id='gooey'>
+                    <feGaussianBlur
+                        in='SourceGraphic'
+                        stdDeviation='5'
+                        result='blur'
+                    />
+                    <feColorMatrix
+                        in='blur'
+                        type='matrix'
+                        values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9'
+                        result='highContrastGraphic'
+                    />
+                    <feComposite
+                        in='SourceGraphic'
+                        in2='highContrastGraphic'
+                        operator='atop'
+                    />
+                </filter>
+            </defs>
+        </svg>
+
+        <span className={styles.bubbles}>
+            {[...Array(10)].map((_, index) => (
+                <span key={index} className={styles.bubble} />
+            ))}
+        </span>
     </Link>
 );
 
