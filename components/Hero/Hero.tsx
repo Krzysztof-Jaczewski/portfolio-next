bfbdc67aa7cd5a31687c5d297fdc78a80877774b
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-06-04 21:00:30 +0200

    dev: implemented scroll button to Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,17 +2,17 @@ import { FC } from 'react';
 import styles from './hero.module.scss';
 import Image from 'next/image';
 import devImage from '/public/images/dev.png';
-import { LinkButton } from 'components/LinkButton';
+import { ScrollButton } from 'components/ScrollButton';
 
 const Hero: FC = () => (
     <div className={styles.header}>
         <div className={styles.intro}>
             <h1>Krzysztof Jaczewski</h1>
             <h2>Frond-end developer</h2>
-            <LinkButton
+            <ScrollButton
                 className={styles.button}
                 text='Wybrane Projekty'
-                link='/#projects'
+                link='projects'
             />
         </div>
         <div className={styles.image}>
