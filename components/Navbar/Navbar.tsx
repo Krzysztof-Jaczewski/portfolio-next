d6cd98003d5bed5b3c1bd244c50eeaa009915107
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-07-18 18:30:52 +0200

    dev: added animation to logo when clicked

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,21 +1,41 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import styles from './navbar.module.scss';
 import Image from 'next/image';
+import clsx from 'clsx';
 
-const Navbar: FC = () => (
-    <header className={styles.header}>
-        <div className={styles.wrapper}>
-            <Link to='hero' duration={500} smooth className={styles.logo}>
-                <Image fill src='/logo.png' alt='' />
-            </Link>
-            <nav className={styles.nav}>
-                <Link to='projects' duration={500} smooth>
-                    Projekty
+const Navbar: FC = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsClicked(false);
+        }, 1000);
+    }, [isClicked]);
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.wrapper}>
+                <Link
+                    to='hero'
+                    duration={500}
+                    offset={-100}
+                    smooth
+                    className={clsx(styles.logo, isClicked && styles.flyout)}
+                    onClick={() => {
+                        setIsClicked((prevState) => !prevState);
+                    }}
+                >
+                    <Image fill src='/logo.png' alt='' />
                 </Link>
-            </nav>
-        </div>
-    </header>
-);
+                <nav className={styles.nav}>
+                    <Link to='projects' duration={500} smooth>
+                        Projekty
+                    </Link>
+                </nav>
+            </div>
+        </header>
+    );
+};
 
 export default Navbar;
