72918c8b81b735f852b21e73cb28e99a31e78a47
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-07-18 13:03:19 +0200

    dev: logo improved

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link rel='icon' href='/logoEye.png' />
-                <meta property='og:image' content='/logoEye.png' />
+                <link rel='icon' href='/logo.png' />
+                <meta property='og:image' content='/logo.png' />
                 <meta property='og:type' content='website' />
             </Head>
             <body>
