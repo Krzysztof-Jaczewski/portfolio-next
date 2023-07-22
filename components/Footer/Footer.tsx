559835b5a5108d978757ee7384d0620feaad6243
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-07-22 20:19:26 +0200

    dev: added color variable

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.scss';
 
 const Footer: FC = () => (
     <footer className={styles.footerWrapper}>
-        <p>Made with passion and love💖 by EyeCode Krzysztof Jaczewski</p>
+        <p>Made with passion and love 💖 by EyeCode Krzysztof Jaczewski</p>
     </footer>
 );
 
