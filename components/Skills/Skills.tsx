abe3bb92dfa902b119f0845e912547c1e4a7106e
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-07-23 22:06:57 +0200

    dev: implemented basic layout to skills

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,29 +1,22 @@
 import { FC } from 'react';
-import styles from './projects.module.scss';
+import styles from './skills.module.scss';
 import { Section } from 'components/Section';
-import { RepoType } from 'modules/getRepositories/types';
-import { Card } from 'components/Card';
+import { skillsSet } from 'configs/skills';
 
-interface Props {
-    repos: RepoType;
-}
-
-const Skills: FC<Props> = ({ repos }) => {
+const Skills: FC = () => {
     return (
         <Section id='skills' title='Umiejętności'>
-            <div className={styles.projectsWrapper}>
-                {repos.items?.map(
-                    ({ fields: { image, description, ...rest } }) => (
-                        <Card
-                            key={rest.title}
-                            image={`https:${image.fields.file.url}`}
-                            description={
-                                description.content[0].content[0].value
-                            }
-                            {...rest}
-                        />
-                    )
-                )}
+            <div className={styles.skillsWrapper}>
+                {Object.entries(skillsSet).map(([key, value]) => (
+                    <div className={styles.skillsBox} key={key}>
+                        <h3>{key}</h3>
+                        <ul className={styles.skillsList}>
+                            {value.map((skill) => (
+                                <li key={skill}>{skill}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
             </div>
         </Section>
     );
