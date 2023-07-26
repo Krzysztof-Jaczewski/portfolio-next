2c87e308720b3e8298df96feef41d607b4eab448
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-07-26 21:51:37 +0200

    dev: modified button

--- components/BubbleButton/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as BubbleButton } from './BubbleButton';
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,6 @@
 import { FC, HTMLProps } from 'react';
 import styles from './bubbleButton.module.scss';
 import clsx from 'clsx';
-import SvgFilter from 'components/BubbleButton/svgfilter';
 
 interface Props extends HTMLProps<HTMLButtonElement> {
     text: string;
@@ -17,12 +16,6 @@ const LinkButton: FC<Props> = ({
 }) => (
     <button className={clsx(styles.button, className)} type={type} {...rest}>
         {text}
-        <SvgFilter />
-        <span className={styles.bubbles}>
-            {[...Array(10)].map((_, index) => (
-                <span key={index} className={styles.bubble} />
-            ))}
-        </span>
     </button>
 );
 
