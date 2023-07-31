dc42bd5a85e29e0349762f02b58233e695eaeac3
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2023-07-31 20:35:07 +0200

    dev: added participated projects

---
+++ components/ParticipatedProjects/ParticipatedProjects.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import styles from './participatedProjects.module.scss';
+import { Section } from 'components/Section';
+import { RepoType } from 'modules/getRepositories/types';
+import Link from 'next/link';
+import { Card } from 'components/Card';
+
+interface Props {
+    repos: RepoType;
+}
+
+const ParticipatedProjects: FC<Props> = ({ repos }) => {
+    return (
+        <Section id='projects' title='Projects'>
+            <div className={styles.projectsWrapper}>
+                {repos.items?.map(
+                    ({ fields: { image, description, ...rest } }) => (
+                        <Card
+                            key={rest.title}
+                            image={`https:${image.fields.file.url}`}
+                            description={
+                                description.content[0].content[0].value
+                            }
+                            {...rest}
+                        />
+                    )
+                )}
+            </div>
+        </Section>
+    );
+};
+
+export default ParticipatedProjects;
---
+++ components/ParticipatedProjects/index.tsx
@@ -0,0 +1,1 @@
+export { default as ParticipatedProjects } from './ParticipatedProjects';
